06ff157c7e4ecf119baca900fc4d1aaa0a701b16
Ashvin0740 <67723298+Ashvin0740@users.noreply.github.com>
2020-09-11 20:03:38 +0530

    StarPattern7
    StarPattern7

---
+++ StarPattern7.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+
+public class StarPattern7 {
+
+
+    public static void main(String[] args)
+    {
+      Scanner sc = new Scanner(System.in);
+
+      System.out.println("Enter the number: ");
+    	int n = sc.nextInt();
+      
+      
+        
+        for (int i=n; i>=1; i--)
+        {
+            for(int j =n; j >i; j--) 
+            {
+            	System.out.print(" ");
+            }
+            for(int k = 1; k< (i*2); k++)
+            {
+            	System.out.print("*");
+            }
+            System.out.println();
+            
+        }
+       
+    }
+}
