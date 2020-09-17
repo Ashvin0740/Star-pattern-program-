667086e54ff823f851798e1e15ec06e4a54920b7
Ashvin0740 <67723298+Ashvin0740@users.noreply.github.com>
2020-09-17 18:57:48 +0530

    starPattern15
    starPattern15

--- StarPattern15.java
+++ StarPattern15.java
@@ -4,19 +4,13 @@ public class StarPattern15
 {
     public static void main(String[] args)
     {
-     // Create a new Scanner object
         Scanner scanner = new Scanner(System.in);
-
-        // Get the number of rows from the user
         System.out.println("Enter the Number  ");
-
-        int rows = scanner.nextInt();
+        int n = scanner.nextInt();
         
-        
-        // Print i number of stars
-        for (int i=1; i<=rows; i++)
+        for (int i=1; i<=n; i++)
         {
-            for (int j = i; j <= rows; j++)
+            for (int j = i; j <= n; j++)
             {
                 System.out.print("*");
             }
@@ -24,20 +18,20 @@ public class StarPattern15
             {
                 System.out.print(" ");
             }
-            for (int l = i; l <= rows; l++)
+            for (int l = i; l <= n; l++)
             {
                 System.out.print("*");
             }
             System.out.println();
         }
         
-        for (int i = 1; i <= rows; i++)
+        for (int i = 1; i <= n; i++)
         {
             for (int j = 1; j <= i; j++)
             {
                 System.out.print("*");
             }
-            for (int k = i*2-2; k < rows*2-2; k++)
+            for (int k = i*2-2; k < n*2-2; k++)
             {
                 System.out.print(" ");
             }
